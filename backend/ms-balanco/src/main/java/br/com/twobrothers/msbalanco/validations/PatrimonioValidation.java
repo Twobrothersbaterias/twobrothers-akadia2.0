ceddb3fb60e71b34c9eaa7d486b6bd514adc9e49
Twobrothersbaterias <twobrothersadm@hotmail.com>
2022-08-18 09:35:51 -0300

    Classe de validation do patrimonio finalizada

--- backend/ms-balanco/src/main/java/br/com/twobrothers/msbalanco/validations/PatrimonioValidation.java
+++ backend/ms-balanco/src/main/java/br/com/twobrothers/msbalanco/validations/PatrimonioValidation.java
@@ -3,19 +3,32 @@ package br.com.twobrothers.msbalanco.validations;
 import br.com.twobrothers.msbalanco.models.dto.PatrimonioDTO;
 import br.com.twobrothers.msbalanco.services.exceptions.InvalidRequestException;
 
-public class PatrimonioValidation {
+import static br.com.twobrothers.msbalanco.utils.RegexPatterns.DATE_REGEX;
 
-    //TODO FINALIZAR CLASSE
+public class PatrimonioValidation {
 
     public boolean validaCorpoDaRequisicao(PatrimonioDTO patrimonio) {
         validaSePossuiAtributosNulos(patrimonio);
-
+        if (patrimonio.getDataAgendamentoPatrimonio() != null) {
+            validaAtributoDataAgendamento(patrimonio.getDataAgendamentoPatrimonio());
+        }
         return true;
     }
 
     public boolean validaSePossuiAtributosNulos(PatrimonioDTO patrimonio) {
-        if (patrimonio.getValorPatrimonio() != null) return true;
-        throw new InvalidRequestException("Validação da despesa falhou. Motivo: um ou mais atributos recebido(s) " +
+        if (patrimonio.getNome() != null
+                && patrimonio.getTipoPatrimonio() != null
+                && patrimonio.getStatusPatrimonio() != null
+                && patrimonio.getValor() != null)
+            return true;
+        throw new InvalidRequestException("Validação do patrimônio falhou. Motivo: um ou mais atributos recebido(s) " +
                 "na requisição são nulos");
     }
+
+    public boolean validaAtributoDataAgendamento(String data) {
+        if (data.matches(DATE_REGEX)) return true;
+        throw new InvalidRequestException
+                ("Validação do patrimoônio falhou. Motivo: o padrão da data de agendamento é inválido");
+    }
+
 }
