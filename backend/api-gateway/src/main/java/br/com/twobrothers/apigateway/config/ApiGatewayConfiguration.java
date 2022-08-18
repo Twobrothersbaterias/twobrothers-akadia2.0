426775a45decdfb31b4fe09fa59a924279557fda
Twobrothersbaterias <twobrothersadm@hotmail.com>
2022-08-18 09:34:40 -0300

    Roteamento do gateway para o micro serviço de balanço patrimonial construído

--- backend/api-gateway/src/main/java/br/com/twobrothers/apigateway/config/ApiGatewayConfiguration.java
+++ backend/api-gateway/src/main/java/br/com/twobrothers/apigateway/config/ApiGatewayConfiguration.java
@@ -14,6 +14,10 @@ public class ApiGatewayConfiguration {
         return builder.routes()
                 .route(p -> p.path("/despesas/**")
                         .uri("lb://ms-despesas"))
+                .route(p -> p.path("/patrimonio/**")
+                        .uri("lb://ms-balanco"))
+                .route(p -> p.path("/vendas/**")
+                        .uri("lb://ms-vendas"))
                 .build();
 
     }
