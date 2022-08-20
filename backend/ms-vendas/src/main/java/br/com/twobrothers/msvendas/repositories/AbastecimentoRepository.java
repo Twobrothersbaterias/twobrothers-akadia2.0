e4c39ed59c0ef442b2410337e32d1dcfaddcf20b
Twobrothersbaterias <twobrothersadm@hotmail.com>
2022-08-20 14:18:48 -0300

    Inseri a query de busca por range de data de cadastro no repositório do abastecimento

--- backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/repositories/AbastecimentoRepository.java
+++ backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/repositories/AbastecimentoRepository.java
@@ -2,8 +2,16 @@ package br.com.twobrothers.msvendas.repositories;
 
 import br.com.twobrothers.msvendas.models.entities.AbastecimentoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface AbastecimentoRepository extends JpaRepository<AbastecimentoEntity, Long> {
+
+    @Query("Select a From AbastecimentoEntity a where a.dataCadastro between ?1 and ?2")
+    List<AbastecimentoEntity> buscaPorRangeDeDataCadastro(LocalDateTime dataInicio, LocalDateTime dataFim);
+
 }
--- backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/repositories/FornecedorRepository.java
+++ backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/repositories/FornecedorRepository.java
@@ -1,8 +1,9 @@
 package br.com.twobrothers.msvendas.repositories;
 
+import br.com.twobrothers.msvendas.models.entities.FornecedorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FornecedorRepository extends JpaRepository<FornecedorRepository, Long> {
+public interface FornecedorRepository extends JpaRepository<FornecedorEntity, Long> {
 }
