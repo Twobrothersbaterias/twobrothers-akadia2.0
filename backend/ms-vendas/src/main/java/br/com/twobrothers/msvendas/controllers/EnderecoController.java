ab0d8e3b8e52bcb653eaa1b26945a755e1be7ad8
Twobrothersbaterias <twobrothersadm@hotmail.com>
2022-08-22 13:49:23 -0300

    Criados os endpoints da classe EnderecoController

---
+++ backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/controllers/EnderecoController.java
@@ -0,0 +1,58 @@
+package br.com.twobrothers.msvendas.controllers;
+
+import br.com.twobrothers.msvendas.models.dto.EnderecoDTO;
+import br.com.twobrothers.msvendas.services.EnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.PathParam;
+import java.util.List;
+
+@RestController
+@RequestMapping("vendas/endereco")
+public class EnderecoController {
+
+    @Autowired
+    EnderecoService service;
+
+    @PostMapping
+    public ResponseEntity<EnderecoDTO> criaNovo(@RequestBody EnderecoDTO endereco) {
+        return ResponseEntity.ok().body(service.criaNovo(endereco));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EnderecoDTO>> buscaTodos() {
+        return ResponseEntity.ok().body(service.buscaTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EnderecoDTO> buscaPorId(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(service.buscaPorId(id));
+    }
+
+    @GetMapping("/pagination")
+    public ResponseEntity<List<EnderecoDTO>> buscaPorPaginacao(
+            @PageableDefault(sort = "dataCadastro", direction = Sort.Direction.ASC, page = 0, size = 5) Pageable paginacao) {
+        return ResponseEntity.ok().body(service.buscaPorPaginacao(paginacao));
+    }
+
+    @GetMapping("/range")
+    public ResponseEntity<List<EnderecoDTO>> buscaPorRangeDeDataDeCadastro(@PathParam("inicio") String inicio, @PathParam("fim") String fim) {
+        return ResponseEntity.ok().body(service.buscaPorRangeDeDataCadastro(inicio, fim));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EnderecoDTO> atualiza(@PathVariable("id") Long id, @RequestBody EnderecoDTO endereco) {
+        return ResponseEntity.ok().body(service.atualizaPorId(id, endereco));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> remove(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(service.deletaPorId(id));
+    }
+
+}
