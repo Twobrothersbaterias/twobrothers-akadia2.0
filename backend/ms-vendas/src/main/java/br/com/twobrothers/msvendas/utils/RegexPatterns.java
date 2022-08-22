3d71154db80477d27879ed9fc917ccdefd7de5b8
Twobrothersbaterias <twobrothersadm@hotmail.com>
2022-08-22 13:48:08 -0300

    Criado os regex para validação do endereço

--- backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/utils/RegexPatterns.java
+++ backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/utils/RegexPatterns.java
@@ -16,6 +16,10 @@ public class RegexPatterns {
             "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)" +
                     "+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
 
+    public static final String POSTAL_CODE_REGEX_PATTERN = "[0-9]{5}-[0-9]{3}";
+
+    public static final String STREET_NUMBER_REGEX_PATTERN = "[0-9]{1,5}";
+
     public static final String CPF_REGEX_PATTERN = "[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}";
     public static final String CPF_REGEX_PATTERN_REPLACED = "[0-9]{11}";
 
