5e52d6f76ca6941986b186122d2ac83bad79f517
Twobrothersbaterias <twobrothersadm@hotmail.com>
2022-08-22 09:47:26 -0300

    Criei os Querys da interface ClienteRepository, que serão utilizadas na classe de serviço

--- backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/repositories/ClienteRepository.java
+++ backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/repositories/ClienteRepository.java
@@ -2,8 +2,29 @@ package br.com.twobrothers.msvendas.repositories;
 
 import br.com.twobrothers.msvendas.models.entities.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
+
+    @Query("Select c From ClienteEntity c where c.dataCadastro between ?1 and ?2")
+    List<ClienteEntity> buscaPorRangeDeDataCadastro(LocalDateTime dataInicio, LocalDateTime dataFim);
+
+    @Query("Select c From ClienteEntity c where c.cpfCnpj = ?1")
+    Optional<ClienteEntity> buscaPorCpfCnpj(String cpfCnpj);
+
+    @Query("Select c From ClienteEntity c where c.email = ?1")
+    Optional<ClienteEntity> buscaPorEmail(String email);
+
+    @Query("Select c From ClienteEntity c where c.telefone = ?1")
+    List<ClienteEntity> buscaPorTelefone(String telefone);
+
+    @Query("Select c From ClienteEntity c where c.nomeCompleto = ?1")
+    List<ClienteEntity> buscaPorNomeCompleto(String nomeCompleto);
+
 }
