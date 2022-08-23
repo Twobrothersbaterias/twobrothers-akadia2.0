c33c6049e56df072b378e4cdea2ab74afd6cabbb
Twobrothersbaterias <twobrothersadm@hotmail.com>
2022-08-23 14:06:36 -0300

    Criei uma query para busca por sigla de um produto cadastrado

--- backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/repositories/ProdutoEstoqueRepository.java
+++ backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/repositories/ProdutoEstoqueRepository.java
@@ -2,8 +2,15 @@ package br.com.twobrothers.msvendas.repositories;
 
 import br.com.twobrothers.msvendas.models.entities.ProdutoEstoqueEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProdutoEstoqueRepository extends JpaRepository<ProdutoEstoqueEntity, Long> {
+
+    @Query("Select p From ProdutoEstoqueEntity p where p.sigla = ?1")
+    Optional<ProdutoEstoqueEntity> buscaPorSigla(String sigla);
+
 }
