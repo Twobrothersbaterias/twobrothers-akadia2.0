135c9d2ca2c5a7367a81b078a2f55fa2aa76e413
Twobrothersbaterias <twobrothersadm@hotmail.com>
2022-08-30 15:13:50 -0300

    Logs da classe PrecoFornecedorValidation finalizados

--- backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/validations/PrecoFornecedorValidation.java
+++ backend/ms-vendas/src/main/java/br/com/twobrothers/msvendas/validations/PrecoFornecedorValidation.java
@@ -2,17 +2,32 @@ package br.com.twobrothers.msvendas.validations;
 
 import br.com.twobrothers.msvendas.exceptions.InvalidRequestException;
 import br.com.twobrothers.msvendas.models.dto.PrecoFornecedorDTO;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class PrecoFornecedorValidation {
 
-    public boolean validaCorpoRequisicao(PrecoFornecedorDTO preco) {
+    public void validaCorpoRequisicao(PrecoFornecedorDTO preco) {
         validaSePossuiAtributosNulos(preco);
-        return true;
+        log.warn("[VALIDAÇÃO - PRECO] Validação do objeto PrecoFornecedorDTO finalizada com sucesso");
     }
 
-    public boolean validaSePossuiAtributosNulos(PrecoFornecedorDTO preco) {
-        if (preco.getValor() != null && preco.getIdUsuarioResponsavel() != null) return true;
-        throw new InvalidRequestException("Requisição inválida. Um ou mais atributos obrigatórios são nulos.");
+    public void validaSePossuiAtributosNulos(PrecoFornecedorDTO preco) {
+
+        log.info("[VALIDAÇÃO - PRECO] Inicializando validação de atributos obrigatórios nulos...");
+        List<String> atributosNulos = new ArrayList<>();
+
+        if (preco.getValor() == null) atributosNulos.add("preco");
+        if (preco.getIdUsuarioResponsavel() == null) atributosNulos.add("idUsuarioResponsavel");
+
+        if (!atributosNulos.isEmpty())
+            throw new InvalidRequestException("Validação do preço falhou. A inserção de um ou mais atributos " +
+                    "obrigatórios é necessária no corpo da requisição: " + atributosNulos);
+
+        log.warn("Validação de atributos nulos OK");
     }
 
 }
