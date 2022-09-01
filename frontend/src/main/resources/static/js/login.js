04a6bed7712644b2f62e6e0a3b6868dd09da92e7
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-09-01 16:12:25 -0300

    Realizei a criação da tela de login do projeto

---
+++ frontend/src/main/resources/static/js/login.js
@@ -0,0 +1,110 @@
+window.onresize = doALoadOfStuff;
+window.onload = responsive()
+
+function responsive(){
+
+	document.getElementById('all').style.display="flex";
+	document.getElementById('main_loader').style.display="none";
+
+	var all = document.getElementById('all')	
+	var body = document.getElementById('body')
+	bodyWidth = document.getElementById('body').clientWidth;
+	bodyHeight = document.getElementById('body').clientHeight;	
+	//body.style.minHeight = '100vh';
+	//all.style.height = document.getElementById('body').clientHeight + 'px';
+
+	var prenchimento = document.getElementById('prenchimento');
+
+	if(bodyHeight < prenchimento.clientHeight){
+		all.style.alignItems="initial";
+		body.style.paddingTop="50px";
+	}
+	else{
+		all.style.alignItems="center";
+		body.style.paddingTop="0";
+	}
+
+	if(bodyWidth > 1200){
+		console.log("Muito grande");
+		document.getElementById('first_hr').style.margin="0 80px";
+		document.getElementById('prenchimento').style.padding="40px 5px";
+
+	}
+
+	else if(bodyWidth <= 1200 && bodyWidth > 992){
+		console.log("Grande");		
+		document.getElementById('first_hr').style.margin="0";
+		document.getElementById('prenchimento').style.padding="50px";
+
+	}
+
+	else if(bodyWidth <= 992 && bodyWidth > 768){
+		console.log('Médio');
+		document.getElementById('first_hr').style.margin="0";
+		document.getElementById('prenchimento').style.padding="50px";
+
+	}
+
+	else if(bodyWidth <= 768 && bodyWidth > 540){
+		console.log('Pequeno');
+		document.getElementById('first_hr').style.margin="0";
+		document.getElementById('prenchimento').style.padding="50px";
+
+
+	}
+	
+	else if(bodyWidth < 540){
+		console.log('Muito pequeno');
+		document.getElementById('first_hr').style.margin="0";
+		document.getElementById('prenchimento').style.padding="50px";
+
+	}							
+}
+
+function usernameInput(){
+	document.getElementById('username').select();
+}
+
+function passwordInput(){
+	document.getElementById('password').select();
+}
+
+function doALoadOfStuff() {
+	responsive()
+}	
+
+function hideMessage(value){
+	if(value==1){
+		document.getElementById('erro').style.display="none";
+	}
+	else if(value==2){
+		document.getElementById('logout').style.display="none";
+	}
+	else{
+		document.getElementById('sucesso').style.display="none";
+	}
+}
+
+function signupEnter(){
+	var signup = document.getElementById('signup');
+	signup.style.color="grey";
+	signup.style.cursor="pointer";
+}
+
+function signupLeave(){
+	var signup = document.getElementById('signup');
+	signup.style.color="#FFFFFF";
+	signup.style.cursor="default";
+}
+
+function loginEnter(){
+	var login = document.getElementById('login_click');
+	login.style.color="grey";
+	login.style.cursor="pointer";
+}
+
+function loginLeave(){
+	var login = document.getElementById('login_click');
+	login.style.color="#FFFFFF";
+	login.style.cursor="default";
+}
